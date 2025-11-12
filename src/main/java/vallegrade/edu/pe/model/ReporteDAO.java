bc56bdac6d504cb29a9408ab9ed52d953e9c697a
Valery Chumpitaz <valery,chumpitaz@vallegrande.edu.pe>
2025-11-12 08:36:35 -0500

    reporteDAO´

--- src/main/java/vallegrade/edu/pe/model/ReporteDAO.java
+++ src/main/java/vallegrade/edu/pe/model/ReporteDAO.java
@@ -9,5 +9,31 @@ public class ReporteDAO {
 
     private final ConexionBD conexion = new ConexionBD();
 
+    public List<Cliente> listarClientes() {
+        List<Cliente> lista = new ArrayList<>();
+        String sql = "SELECT id_cliente, nombre, correo, telefono FROM clientes";
+
+
+        try (Connection cn = conexion.conectar();
+             PreparedStatement ps = cn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+
+            while (rs.next()) {
+                Cliente c = new Cliente(
+                        rs.getInt("id_cliente"),
+                        rs.getString("nombre"),
+                        rs.getString("correo"),
+                        rs.getString("telefono")
+                );
+                lista.add(c);
+            }
+
+
+        } catch (SQLException e) {
+            System.err.println("Error al listar clientes: " + e.getMessage());
+        }
+        return lista;
+    }
 
 }
