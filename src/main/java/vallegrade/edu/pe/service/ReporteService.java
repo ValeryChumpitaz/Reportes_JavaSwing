763dee76d72cd47f2b1354857532e120c91c558b
Valery Chumpitaz <valery,chumpitaz@vallegrande.edu.pe>
2025-11-12 08:51:15 -0500

    reporteService

--- src/main/java/vallegrade/edu/pe/service/ReporteService.java
+++ src/main/java/vallegrade/edu/pe/service/ReporteService.java
@@ -1,5 +1,6 @@
 package vallegrade.edu.pe.service;
 
+import com.lowagie.text.pdf.PdfTable;
 import vallegrade.edu.pe.model.Cliente;
 import vallegrade.edu.pe.model.ReporteDAO;
 
@@ -38,5 +39,41 @@ public class ReporteService {
         return modelo;
     }
 
+    public void exportarPDF(JTable tabla) {
+        String ruta = "Reportes/Reporte_Clientes_Profesional.pdf";
+        new File("Reportes").mkdirs();
 
+
+        try (FileOutputStream archivo = new FileOutputStream(ruta)) {
+            Document doc = new Document(PageSize.A4.rotate(), 36, 36, 54, 36);
+            PdfWriter writer = PdfWriter.getInstance(doc, archivo);
+            doc.open();
+
+
+            // --- ENCABEZADO ---
+            PdfPTable header = new PdfPTable(2);
+            header.setWidthPercentage(100);
+            header.setWidths(new int[]{1, 3});
+
+
+            // Logo (opcional)
+            try {
+                Image logo = Image.getInstance("src/main/resources/logo.png"); // pon tu logo aquí
+                logo.scaleToFit(80, 80);
+                PdfPCell cellLogo = new PdfPCell(logo, false);
+                cellLogo.setBorder(Rectangle.NO_BORDER);
+                cellLogo.setHorizontalAlignment(Element.ALIGN_LEFT);
+                header.addCell(cellLogo);
+            } catch (Exception e) {
+                PdfPCell empty = new PdfPCell(new Phrase(""));
+                empty.setBorder(Rectangle.NO_BORDER);
+                header.addCell(empty);
+            }
+
+
+            // Título
+            PdfPCell cellTitle = new PdfPCell();
+            Paragraph titulo = new Paragraph("REPORTE DE CLIENTES\n",
+        }
+    }
 }
