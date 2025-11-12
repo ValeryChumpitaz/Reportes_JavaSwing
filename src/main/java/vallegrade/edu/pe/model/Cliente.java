f1acbfbaa822b829870636b17a14494d4d1ee2f1
Valery Chumpitaz <valery,chumpitaz@vallegrande.edu.pe>
2025-11-12 08:28:36 -0500

    modelo cliente

--- src/main/java/vallegrade/edu/pe/model/Cliente.java
+++ src/main/java/vallegrade/edu/pe/model/Cliente.java
@@ -2,4 +2,29 @@ package vallegrade.edu.pe.model;
 
 public class Cliente {
 
+    private int idCliente;
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    public Cliente(){
+    }
+    public Cliente(int idCliente, String nombre, String correo, String telefono) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
 }
